f22639696296a4a1e5882227dd272e29039df51c
Tiramister <nh111424@gmail.com>
2021-09-09 20:45:42 +0900

    Initial Commit

---
+++ src/main/java/net/tiramister/integer/IntegerInformationApplication.java
@@ -0,0 +1,11 @@
+package net.tiramister.integer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegerInformationApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(IntegerInformationApplication.class, args);
+  }
+}
---
+++ src/test/java/net/tiramister/integer/IntegerInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tiramister.integer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegerInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
